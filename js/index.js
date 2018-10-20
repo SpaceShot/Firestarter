095e49c8a65f1e6140385fa601614bbeec9d0cc2
Chris Gomez <cagomez@cellbycell.com>
2018-10-20 13:10:40 -0400

    Continously watching position / show last readings

--- js/index.js
+++ js/index.js
@@ -1,13 +1,16 @@
 $(function () {
     if ("geolocation" in navigator) {
         navigator.geolocation;
-        $('#btnGetCoordinates').click(getGPSCoordinates);
+        // $('#btnGetCoordinates').click(getGPSCoordinates);
+        getGPSCoordinates();
     }
     else {
         $('#warning').text('No access to geolocation data');
         $('#warning').setAttr('hidden', false);
     }
 });
+
+var watchId = null;
 
 function getGPSCoordinates() {
     let numVars = arguments.length;
@@ -18,14 +21,40 @@ function getGPSCoordinates() {
         timeout: 27000
     };
 
-    navigator.geolocation.getCurrentPosition(function (position) {
-        setTimeout(displayCoordinates(position.coords), 0);
-    }, null, geo_options);
+    navigator.geolocation.watchPosition(
+        function (position) {
+            onGotCoordinates(position.coords)
+        },
+        null,
+        geo_options);
+}
+
+function onGotCoordinates(coords)
+{
+    displayReading(coords);
+    displayCoordinates(coords);
 }
 
 function displayCoordinates(coords) {
     $('#latitude').text(coords.latitude);
     $('#longitude').text(coords.longitude);
     $('#accuracy').text(coords.accuracy);
+}
+
+function displayReading(coords)
+{
+    //$('#readingsList').append('<li>Reading taken at: ' + new Date(Date.now()) + '. Lat:</li>')
+    $('#readingsList').append(`<li>Reading taken at: ${new Date(Date.now())}. Lat:${coords.latitude} Long:${coords.longitude} Acc:${coords.accuracy}</li>`)
+    let theNumber = $("#readingsList li").length
+    while (theNumber > 5) {
+        $("#readingsList li").first().detach()
+        theNumber = $("#readingsList li").length
+    }
+    $('#readingsCount').text(theNumber)
+}
+
+function formatDate(date)
+{
+    return date.getHours() + ':'
 }
 
