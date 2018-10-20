9ca02f4fb0020428421d688ad08b6289a7c791ea
Chris Gomez <cagomez@cellbycell.com>
2018-10-20 14:39:50 -0400

    Added map with marker on current location

--- js/index.js
+++ js/index.js
@@ -11,6 +11,7 @@ $(function () {
 });
 
 var watchId = null;
+var mapCreated = false;
 
 function getGPSCoordinates() {
     let numVars = arguments.length;
@@ -21,7 +22,7 @@ function getGPSCoordinates() {
         timeout: 27000
     };
 
-    navigator.geolocation.watchPosition(
+    watchId = navigator.geolocation.watchPosition(
         function (position) {
             onGotCoordinates(position.coords)
         },
@@ -29,10 +30,13 @@ function getGPSCoordinates() {
         geo_options);
 }
 
-function onGotCoordinates(coords)
-{
+function onGotCoordinates(coords) {
     displayReading(coords);
     displayCoordinates(coords);
+    if (!mapCreated) {
+        createMap(coords.latitude, coords.longitude);
+        mapCreated = true;
+    }
 }
 
 function displayCoordinates(coords) {
@@ -41,9 +45,7 @@ function displayCoordinates(coords) {
     $('#accuracy').text(coords.accuracy);
 }
 
-function displayReading(coords)
-{
-    //$('#readingsList').append('<li>Reading taken at: ' + new Date(Date.now()) + '. Lat:</li>')
+function displayReading(coords) {
     $('#readingsList').append(`<li>Reading taken at: ${new Date(Date.now())}. Lat:${coords.latitude} Long:${coords.longitude} Acc:${coords.accuracy}</li>`)
     let theNumber = $("#readingsList li").length
     while (theNumber > 5) {
@@ -53,8 +55,34 @@ function displayReading(coords)
     $('#readingsCount').text(theNumber)
 }
 
-function formatDate(date)
-{
+function formatDate(date) {
     return date.getHours() + ':'
+}
+
+function createMap(lat, long) {
+    var mymap = L.map('mapid').setView([lat, long], 13);
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+            '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        id: 'mapbox.streets'
+    }).addTo(mymap);
+
+    L.marker([lat, long]).addTo(mymap);
+
+    // L.circle([51.508, -0.11], {
+    //     color: 'red',
+    //     fillColor: '#f03',
+    //     fillOpacity: 0.5,
+    //     radius: 500
+    // }).addTo(mymap);
+
+    // L.polygon([
+    //     [51.509, -0.08],
+    //     [51.503, -0.06],
+    //     [51.51, -0.047]
+    // ]).addTo(mymap);
 }
 
